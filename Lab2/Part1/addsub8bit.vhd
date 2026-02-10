57bf8f9df5beed70f5084b62f958b7e9b3761f98
Hmm <gbreeze.study@gmail.com>
2026-02-09 20:40:08 -0800

    Add files via upload
    made it so that latching happens for a and b under a single process

--- Lab2/Part1/addsub8bit.vhd
+++ Lab2/Part1/addsub8bit.vhd
@@ -4,37 +4,33 @@ use ieee.numeric_std.all;
 
 --entity declaration
 entity addsub8bit is
-	port
-	(
-		sw : in std_logic_vector(9 downto 0);
-		key : in std_logic_vector(1 downto 0);
-		ledr0 : out std_logic_vector(7 downto 0)
+	port(
+	sw : in std_logic_vector(9 downto 0);
+	key : in std_logic_vector(1 downto 0);
+	ledr0 : out std_logic_vector(7 downto 0)
 	);
 end addsub8bit;
 
 --architecture declaration
 architecture behaviour of addsub8bit is
- signal a, b : unsigned(7 downto 0);
-	 signal bMinus : unsigned(7 downto 0); --if sw9 is set to subtract, add this instead of b
-	 signal carry : std_logic;
-	 signal sum : unsigned(8 downto 0);
-begin
+	signal a, b : unsigned(7 downto 0);
+	signal bMinus : unsigned(7 downto 0); --if sw9 is set to subtract, add this instead of b
+	signal carry : std_logic;
+	signal sum : unsigned(8 downto 0);
+	--signal key0_prev, key1_prev : std_logic := '1'; --assume the previous key state is 1
 
---latch a from key1 press
-process (key(1))
 begin
-	if falling_edge(key(1)) then
-		a <= unsigned(sw(7 downto 0));
-	end if;
-end process;
-
---latch b from key0 press
-process (key(0))
-begin
-	if falling_edge(key(0)) then
-		b <= unsigned(sw(7 downto 0));
-	end if;
-end process;
+	process (key) --latch a and b from key1 and key0 press
+	begin
+		if key(1) = '0' then
+			a <= unsigned(sw(7 downto 0));
+		end if;
+		if key(0) = '0' then
+			b <= unsigned(sw(7 downto 0));
+		end if;
+		--key0_prev <= key(0); --reset previous key prev states
+		--key1_prev <= key(1);
+	end process;
 
 --add/suntract selection from sw9 state
 bMinus <= b when sw(9) = '0' else not b;
