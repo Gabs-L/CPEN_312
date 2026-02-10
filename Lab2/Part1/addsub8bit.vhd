f263aec25e71eda97930f7cfddd55f5ae5d92c60
Hmm <gbreeze.study@gmail.com>
2026-02-09 22:29:51 -0800

    Add files via upload
    Added negative handling (returns absolute value of any operation so 4-8 = 4)

--- Lab2/Part1/addsub8bit.vhd
+++ Lab2/Part1/addsub8bit.vhd
@@ -2,8 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
---entity declaration
-entity addsub8bit is
+entity addsub8bit is --entity declaration
 	port(
 	sw : in std_logic_vector(9 downto 0);
 	key : in std_logic_vector(1 downto 0);
@@ -11,29 +10,21 @@ entity addsub8bit is
 	);
 end addsub8bit;
 
---architecture declaration
-architecture behaviour of addsub8bit is
-	signal a, b : unsigned(7 downto 0) := (others => '0');
-	signal bMinus : unsigned(7 downto 0); --if sw9 is set to subtract, add this instead of b
-	signal carry : std_logic;
-	signal sum : unsigned(8 downto 0);
+architecture behaviour of addsub8bit is --architecture declaration
+	signal a : signed(7 downto 0);
+	signal b : signed(7 downto 0);
+	signal output : signed(8 downto 0); --extra bit for carry
 
 begin
 	process (key) --latch a and b from key1 and key0 press
 	begin
 		if key(1) = '0' then
-			a <= unsigned(sw(7 downto 0));
+			a <= signed(sw(7 downto 0));
 		end if;
 		if key(0) = '0' then
-			b <= unsigned(sw(7 downto 0));
+			b <= signed(sw(7 downto 0));
 		end if;
 	end process;
-
---add/suntract selection from sw9 state
-bMinus <= b when sw(9) = '0' else not b;
-carry <= sw(9);
-sum <= ('0'&a) + ('0'&bMinus) + unsigned'("0000000" & carry);
-
---displaying result
-ledr0 <= std_logic_vector(sum(7 downto 0));
+output <= ('0' & a) + ('0' & (not b) + 1) when sw(9) = '1' else ('0' & a) + ('0' & b);
+ledr0 <= std_logic_vector(abs(output(7 downto 0))); --displaying absolute value of result
 end architecture behaviour;
